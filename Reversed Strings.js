5100af9a4b05584e21981a5c2d50044ea953dd18
Aliaksei Redzko <aleha.redko@gmail.com>
2025-07-26 10:14:01 +0300

    Reversed Strings

---
+++ Reversed Strings.js
@@ -0,0 +1,14 @@
+// Complete the solution so that it reverses the string passed into it.
+
+// 'world'  =>  'dlrow'
+// 'word'   =>  'drow'
+
+
+
+//Solution
+
+function solution(str) {
+    return arrayReverse = str.split('').reverse().join("")
+}
+
+console.log(solution('world'));
