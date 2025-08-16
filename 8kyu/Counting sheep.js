dd0c8f32dfe6ce8d897fa79cd8053731aed22681
Aliaksei Redzko <aleha.redko@gmail.com>
2025-08-16 22:01:37 +0300

    add two option answer for Counting sheep 8kuy

---
+++ 8kyu/Counting sheep.js
@@ -0,0 +1,33 @@
+// Consider an array / list of sheep where some sheep may be missing from their place.We need a function that counts the number of sheep present in the array(true means present).
+
+// For example,
+
+//     [true, true, true, false,
+//         true, true, true, true,
+//         true, false, true, false,
+//         true, false, false, true,
+//         true, true, true, true,
+//         false, false, true, true]
+// The correct answer would be 17.
+
+// Hint: Don't forget to check for bad values like null/undefined
+
+
+//solution
+
+// function countSheeps(sheep) {
+//     let count = 0;
+//     for (let i = 0; i < sheep.length; i++) {
+//         const element = sheep[i]
+//         if (element === true) {
+//             count++
+//         }
+//     }
+//     return count
+// }
+
+function countSheeps(sheep) {
+    const newArray = sheep.filter((element) => element === true)
+
+    return newArray.length
+}
