bf4382655e79046472e4f50a5b20c09e87824542
AliakseiRedzko <aleha.redko@gmail.com>
2025-12-21 19:43:23 +0300

    rock paper scissors 8kyu

---
+++ 8kyu/Rock Paper Scissors!.js
@@ -0,0 +1,38 @@
+// Rules of the "Rock, Paper, Scissors" game are:
+//
+//     Rock beats Scissors,
+//     Scissors beat Paper,
+//     Paper beats Rock,
+//     Two identical moves are a draw.
+//     Let's play! You will be given valid moves of two Rock, Paper, Scissors players, and have to return which player won: "Player 1 won!" for player 1, and "Player 2 won!" for player 2. In case of a draw return Draw!.
+//
+// Examples:
+//     "scissors",     "paper"     --> "Player 1 won!"
+// "scissors",     "rock"      --> "Player 2 won!"
+// "paper",        "paper"     --> "Draw!"
+
+
+//solution
+const rps = (p1, p2) => {
+    const rock = 0
+    const paper = 1
+    const scissors = 2
+
+    if(p1 === p2){
+        return ("Draw!")
+    } else if((p1 === "scissors" && p2 === "paper") || (p1 === "rock" && p2 === "scissors") || (p1 === "paper" && p2 === "rock")){
+        return  "Player 1 won!"
+    } else{
+        return "Player 2 won!"
+    }
+}
+
+
+
+
+
+
+
+
+
+
