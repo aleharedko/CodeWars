7efeb51062db41c2a73bc3a50ccbeefd989b70ab
AliakseiRedzko <aleha.redko@gmail.com>
2025-12-21 18:03:56 +0300

    reduce beginner 8kyu

---
+++ 8kyu/Beginner - Reduce but Grow.js
@@ -0,0 +1,14 @@
+//Given a non-empty array of integers, return the result of multiplying the values together in order. Example
+
+//solution
+
+// let a = [1, 2, 3, 4];
+// function grow(x){
+//     return x.reduce((acc,rec) => acc * rec)
+// };
+//
+// console.log(grow(a))
+
+function grow(x){
+    return x.reduce((acc,rec) => acc * rec)
+};
