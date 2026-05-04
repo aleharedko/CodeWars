21ce0d1902c9d8586ba6401c611b3d2a7f47b899
AliakseiRedzko <aleha.redko@gmail.com>
2026-05-04 22:08:42 +0300

    add prod 8 kyu

---
+++ 8kyu/Duck Duck Goose.js
@@ -0,0 +1,27 @@
+// Given an array of Player objects and a position (first position is 1), return the name of the chosen Player.
+//     name is a property of Player objects, e.g Player.name
+//
+// Example:
+//
+//     duck_duck_goose([a, b, c, d], 1) should return a.name
+// duck_duck_goose([a, b, c, d], 5) should return a.name
+// duck_duck_goose([a, b, c, d], 4) should return d.name
+// Random input limits:
+//
+//     6
+// ≤
+// Players
+// ≤
+// 50
+// 6≤Players≤50
+// 5000
+// ≤
+// Position
+// ≤
+// 50000
+// 5000≤Position≤50000
+
+//solution
+function duckDuckGoose(players, goose) {
+
+}
